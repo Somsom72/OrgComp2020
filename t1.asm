1b8f7cf15ba023fdabde561aaa3d6ca7d1016ebe
daltonsato <54639674+daltonsato@users.noreply.github.com>
2020-03-27 23:34:57 -0300

    IMC e tabuada prontos

--- t1.asm
+++ t1.asm
@@ -4,11 +4,14 @@
 #Strings para interação com o usuario
 str_erro : .asciiz "Valor digitado invalido\n\n"
 str_menu : .asciiz "Bem-vindo à calculadora\n"
-str_opera: .asciiz "Digite:\n\n1-soma\n2-subtracao\n3-multiplicacao\n4-divisao\n5-potencia\n6-raiz quadrada\n7-tabuada\n8-IMC\n9-fatorial\n10-fibonacci\n11-Encerrar o programa\n\n"
+str_opera: .asciiz "\n\nDigite:\n1-soma\n2-subtracao\n3-multiplicacao\n4-divisao\n5-potencia\n6-raiz quadrada\n7-tabuada - Numero de 1 a 10\n8-IMC - Altura Peso\n9-fatorial\n10-fibonacci\n11-Encerrar o programa\n\n"
 str_close: .asciiz "\n----Fechando programa----\n"
 str_args: .asciiz "Insira o(s) argumento(s)\n"
+str_Altura: .asciiz "Altura: "
+str_Peso: .asciiz "Peso: "
 str_resp: .asciiz "Resposta: "
-str_novaLinha: .asciiz "\n"
+str_espaco: .asciiz " "
+str_novaLinha: .asciiz "\n\n"
 
 	.text
 	.globl main
@@ -53,7 +56,10 @@ menu:
 	beq $t1, 6, raizMain
 	
 	#OPCAO 7
+	beq $t1, 7, tabuada
+	
 	#OPCAO 8
+	beq $t1, 8, IMC
 	
 	#OPCAO 9
 	beq $t1, 9, fatorialMain
@@ -114,7 +120,99 @@ raizImprimir:
 	la $a0, str_novaLinha
 	syscall
 	j menu
-		
+	
+tabuada:
+	#Mensagem que pede argumento
+	li $v0, 4
+	la $a0, str_args
+	syscall
+	
+	#Configuracao para leitura de inteiro
+	li $v0, 5
+	syscall
+	
+	
+	#Comparacao para validade do numero digitado (maior que 10)
+	addi $t1, $0, 11
+	bge $v0, $t1, erro
+	
+	#Comparaçao para validade do numero digitado (menor que 0)
+	addi $t1, $0, 0
+	ble $v0, $t1, erro
+	
+	#Copia valor digitado para registrador temporario
+	move $t4, $v0
+	addi $t5, $0, 11
+	
+tabuada_mul:
+
+	#Branch de saída da tabuada (quando ja chegou na multiplicacao por 10)
+	bge $t1, $t5, menu
+
+	#Multiplica o valor lido por 0, 1, 2, ... (que esta em $t1)	
+	mul $t2, $t4, $t1
+	
+	#Copia o valor ja multiplicado para $a0, preparando para impressao
+	move $a0, $t2
+	
+	#Comando de impressao de inteiro
+	addi $v0, $0, 1
+	syscall
+	
+	#Printando um espaco entre os numeros da tabuada
+	addi $v0, $0, 4
+	la $a0, str_espaco
+	syscall
+	
+	#Incrementa o contador para comparacao
+	addi $t1, $t1, 1
+	
+	#Jump incondicional para loop's do calculo
+	j tabuada_mul
+
+IMC:
+	#Imprime string de mensagem
+	li $v0, 4
+	la $a0, str_args
+	syscall
+
+	#Chamada do sistema para leitura de double - Altura
+	li $v0, 4
+	la $a0, str_Altura
+	syscall
+	
+	li $v0, 7
+	syscall
+	mov.d $f2, $f0
+	
+	#Chamada do sistema para leitura de double - Peso
+	li $v0, 4
+	la $a0, str_Peso
+	syscall
+	
+	li $v0, 7
+	syscall
+	mov.d $f4, $f0
+	
+	#IMC => peso/(altura^2)
+	mul.d $f6, $f2, $f2
+	div.d $f12, $f4, $f6
+	
+	#Impressao de double e chamada do sistema
+	#Impressao de double necessita valor a ser impresso no registrador $f12
+	li $v0, 4
+	la $a0, str_resp
+	syscall
+	
+	li $v0, 3
+	syscall
+	
+	li $v0, 4
+	la $a0, str_novaLinha
+	syscall
+	
+	j menu
+	
 fatorialMain:
 	#Pedindo argumento
 	li $v0, 4
@@ -171,5 +269,3 @@ fatorialLoop:
 	bgt $t2, 1, fatorialLoop
 	
 	jr $ra
-	
-	
