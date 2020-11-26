d57b615f575331e8dad663657e117e43b88f66bb
NathanTBP <48016418+NathanTBP@users.noreply.github.com>
2020-11-26 16:59:26 -0300

    Add files via upload

--- SO_2SEM/EntradaSaida.c
+++ SO_2SEM/EntradaSaida.c
@@ -1,1 +1,52 @@
 #include "EntradaSaida.h"
+
+int main(void){
+
+    char opcao=0;
+
+    int i,j, placaratual;
+
+    placaratual=20;
+
+    system ("/bin/stty raw");
+
+    while(opcao!='q'){
+
+        opcao=getchar();
+
+        if(opcao == 'a') placaratual++;
+        else if(opcao == 'b') placaratual--;
+
+        system ("clear");
+
+        printf("\ri");
+
+        for(i=0;i<placaratual;i++) printf(" ");
+
+        printf("|");
+
+        for(j=0;j<10;j++) printf("-");
+        
+        printf("|");
+
+        for(;i<40;i++) printf(" ");
+
+        printf("f\r\n");
+        
+        printf("\rPlacaratual: %d\r\n",placaratual);
+
+        if(placaratual==0){
+            printf("\rAzul venceu!!\n");
+            opcao='q';
+        }
+        else if(placaratual==40){
+            printf("\rVermelho venceu!!\n");
+            opcao='q';
+        }
+
+    }
+
+    system ("/bin/stty cooked");
+
+    return 0;
+}
--- SO_2SEM/EntradaSaida.h
+++ SO_2SEM/EntradaSaida.h
@@ -4,6 +4,18 @@
  */
 
 #ifndef ENTRADASAIDA_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define boolean unsigned char
+#define TRUE 1
+#define FALSE 0
+
+#define max(a,b) ((a) > (b) ? (a) : (b))
+#define min(a,b) (((a)<(b))?(a):(b))
+
 #define ENTRADASAIDA_H
 
 #endif
