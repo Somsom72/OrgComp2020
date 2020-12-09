980465ab98ce32431fd698f00d7e98f2afc586d0
daltonsato <daltonhiroshisato@hotmail.com>
2020-12-09 18:04:11 -0300

    Agora ele limpa o buffer pra proxima lida TODO: corrigir problema de print do cabo

--- SO_2SEM/EntradaSaida.c
+++ SO_2SEM/EntradaSaida.c
@@ -4,9 +4,12 @@
 void captaEntrada(int* pontuacao1, int* pontuacao2){
 
     char opcao;
+	char lixo;
 
 	system ("/bin/stty raw -echo");
     opcao = getchar();
+	scanf("%c", &lixo);
+	fflush(stdin);
 	
 	system ("/bin/stty cooked");
 
@@ -22,15 +25,16 @@ void imprimeSaida(int* placar, char *nome1, char *nome2){
 
     system ("clear");
 
-    printf("O cabo-de-guerra está valendo!\n\nPressione 's' para puxar o cabo para a esquerda ou 'k' para puxá-lo para a direita\n\n\n");
+    printf("\nO cabo-de-guerra está valendo!\n\nPressione 's' para puxar o cabo para a esquerda ou 'k' para puxá-lo para a direita\n\n\n\n");
 
-    printf("\rs");
+    printf("\r%s", nome1);
 
     if(placar<0){
-        for(i=-1*DURACAO_DO_JOGO;i<*placar;i++) printf(" ");
+		printf("kk");
+        for(i=DURACAO_DO_JOGO + (*placar);i>0;i--) printf(" ");
         printf("|"); for(i=0;i<TAMANHO_DA_CORDA;i++) printf("-"); printf("|");
         for(i=0;i<DURACAO_DO_JOGO;i++) printf(" ");
-        for(i=0;i<abs(*placar);i++) printf(" ");
+        //for(i=0;i<abs(*placar);i++) printf(" ");
     } 
 
     else{
@@ -40,11 +44,11 @@ void imprimeSaida(int* placar, char *nome1, char *nome2){
         for(i=(int)*placar;i<DURACAO_DO_JOGO;i++) printf(" ");
     } 
 
-    printf("k\r\n");
+    printf("%s\r\n", nome2);
 
-    if(*placar==0) printf("\rPlacarAtual: 0. Está tudo empatado!\r\n\n");
-    else if(*placar<0) printf("\rPlacarAtual: %d de vantagem para %s\r\n\n",-(*placar), nome1);
-    else printf("\rPlacarAtual: %d de vantagem para %s\r\n\n",(*placar), nome2);
+    if(*placar==0) printf("PlacarAtual: 0. Está tudo empatado!\n\n");
+    else if(*placar<0) printf("PlacarAtual: %d de vantagem para %s\n\n",-(*placar), nome1);
+    else printf("PlacarAtual: %d de vantagem para %s\n\n",(*placar), nome2);
 
     return;
 }
